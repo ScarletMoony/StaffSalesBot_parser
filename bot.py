22960d3829580dac3b6391e484f202ba62e4b10f
ScarletMoon <alexgameshow1@gmail.com>
2022-04-20 20:57:27 +0300

    Just first commit

---
+++ bot.py
@@ -0,0 +1,9 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+
+print('some')
+
+# if __name__ == '__main__':
+#     executor.start_polling()
